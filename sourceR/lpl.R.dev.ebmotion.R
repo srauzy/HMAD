d5e8473646a4b524440ff5a554cc2b7936e5379f
srauzy <38458910+srauzy@users.noreply.github.com>
2023-06-12 11:07:13 +0200

    License added

--- sourceR/lpl.R.dev.ebmotion.R
+++ sourceR/lpl.R.dev.ebmotion.R
@@ -1,3 +1,25 @@
+# -------------------------------------------------------------------------
+# Copyright (C) 2018-2023  Stéphane Rauzy
+# Laboratoire Parole et Langage, Aix-en-Provence, France
+#
+# Use of this software is governed by the GNU Public License, version 3.
+#
+# HMAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HMAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HMAD If not, see <http://www.gnu.org/licenses/>.
+#
+# This banner notice must not be removed.
+# -------------------------------------------------------------------------
+
 ## lpl.R.dev.ebmotion.R, creator S. Rauzy, LPL, 05/10/2016
 
 
